e6e1c5534c4bb3286d9bbbd70a9ea7c47ea2d674
goreleaserbot <goreleaser@carlosbecker.com>
2018-03-09 10:42:20 +0100

    config-bob version 0.4.0

--- config-bob.rb
+++ config-bob.rb
@@ -1,9 +1,9 @@
 class ConfigBob < Formula
   desc "CLI utility to generate secure configurations"
   homepage "https://github.com/foomo/config-bob"
-  url "https://github.com/foomo/config-bob/releases/download/0.3.3/config-bob_0.3.3_darwin_amd64.tar.gz"
-  version "0.3.3"
-  sha256 "946ffa6281dd9a051b726984ec166d21082eec7078162b7650ef044624ab51e6"
+  url "https://github.com/foomo/config-bob/releases/download/0.4.0/config-bob_0.4.0_darwin_amd64.tar.gz"
+  version "0.4.0"
+  sha256 "f0a5c29fba7be65dd3135832bfb28222e0560b3d9c9dc4e66b2aa8781d964ebb"
 
   def install
     bin.install "config-bob"
@@ -11,5 +11,9 @@ class ConfigBob < Formula
 
   def caveats
     "config-bob -h"
+  end
+
+  test do
+    
   end
 end
