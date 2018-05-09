eea4cbe4aeb5b5ae4d6ad6348f672d0e296c30d1
goreleaserbot <goreleaser@carlosbecker.com>
2018-05-09 14:26:05 +0200

    Brew formula update for config-bob version 0.4.5

--- config-bob.rb
+++ config-bob.rb
@@ -1,19 +1,16 @@
 class ConfigBob < Formula
   desc "CLI utility to generate secure configurations"
   homepage "https://github.com/foomo/config-bob"
-  url "https://github.com/foomo/config-bob/releases/download/0.4.3/config-bob_0.4.3_darwin_amd64.tar.gz"
-  version "0.4.3"
-  sha256 "9f567c1bc18088a236a802f7cca421118c5e66a483d823f78df73a9b593f88ad"
+  url "https://github.com/foomo/config-bob/releases/download/0.4.5/config-bob_0.4.5_darwin_amd64.tar.gz"
+  version "0.4.5"
+  sha256 "ea18322ea3f1aa4e292432dc0ff46071a7b8da120231d09a5fab21b7f77488b6"
 
   def install
     bin.install "config-bob"
   end
 
-  def caveats
-    "config-bob -h"
-  end
-
-  test do
-    
+  def caveats; <<~EOS
+    config-bob -h
+  EOS
   end
 end
